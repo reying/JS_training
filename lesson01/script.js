40a9615d953d8fe630d061736c11248ecfed00e8
Ivanov Dmitriy <rey.veritas@yandex.ru>
2021-01-29 00:45:34 +0500

    Добавил переменны

---
+++ lesson01/script.js
@@ -0,0 +1,11 @@
+let money;
+let income;
+
+function addExpenses() {};
+let deposit;
+const mission = 1;
+const period = 5;
+
+alert('Жмякни чтобы продолжить --->');
+
+console.log('Скрипт продолжил выполняться');
